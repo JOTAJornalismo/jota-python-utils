c38248670683db1501bc8d0bd35e222ed438a063
Lucas Facchini <lucas.cf@live.com>
2023-07-24 16:14:58 -0300

    Fix doc & tests

--- jota_utils/datetime.py
+++ jota_utils/datetime.py
@@ -6,8 +6,8 @@ from dateutil.parser import parse
 def utc_to_local(utc_date, local_tz):
     """Converts a UTC datetime to a local datetime.
 
-    Ex: str(utc_to_local('2019-01-01T00:00:00', pytz.timezone('America/Sao_Paulo')))
-        -> '2018-12-31 22:00:00-03:00'
+    Ex: str(utc_to_local('2023-07-24T19:00:00', pytz.timezone('America/Sao_Paulo')))
+        -> '2023-07-24 16:00'
     """
 
     return parse(utc_date).replace(tzinfo=pytz.utc).astimezone(local_tz)
--- tests/test_datetime.py
+++ tests/test_datetime.py
@@ -10,11 +10,11 @@ class UtcToLocalTests(TestCase):
     tz = pytz.timezone('America/Sao_Paulo')
 
     def test_with_iso_date(self):
-        self.assertIn('2018-12-31 22:00', str(utc_to_local('2019-01-01T00:00:00', self.tz)))
-        self.assertIn('2019-01-01 20:00', str(utc_to_local('2019-01-01T22:00:00', self.tz)))
+        self.assertIn('2023-07-24 16:00', str(utc_to_local('2023-07-24T19:00:00', self.tz)))
+        self.assertIn('2023-01-01 19:00', str(utc_to_local('2023-01-01T22:00:00', self.tz)))
 
     def test_without_iso_date(self):
-        self.assertIn('2018-12-31 22:00', str(utc_to_local('2019-01-01 00:00:00', self.tz)))
+        self.assertIn('2023-07-24 16:00', str(utc_to_local('2023-07-24 19:00', self.tz)))
 
     def test_invalid(self):
         self.assertRaises(TypeError, utc_to_local, None, self.tz)
